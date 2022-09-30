1aaffc66669636f64273e762f44ba14f1aff3d75
Rover Chen <rover.k.chen@gmail.com>
2022-09-30 09:08:56 +0800

    import * as React from 'react';
        export const ReactComponent: React.FunctionComponent<React.SVGProps<
            SVGSVGElement
        > & { title?: string }>;
    
        const src: string;
        export default src;

--- packages/web/src/types/css/css.d.ts
+++ packages/web/src/types/css/css.d.ts
@@ -2,3 +2,13 @@ declare module '*.css' {
   const content: { [className: string]: string }
   export default content
 }
+
+declare module '*.svg' {
+
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+
+  const content: string;
+  export default content;
+}
